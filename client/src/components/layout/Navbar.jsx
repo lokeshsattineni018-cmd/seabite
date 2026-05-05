abfa8d955586af6c616d71f0fc0a3c75b57a61aa
Lokesh Sattineni <lokeshsattineni018@gmail.com>
2026-05-05 22:30:43 +0530

    fix: navbar syntax error (missing closing brace)

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -497,6 +497,7 @@ export default function Navbar({ announcementActive = false }) {
                   )}
                 </AnimatePresence>
               </div>
+              )}
 
               {/* Notifications */}
               {user && (
