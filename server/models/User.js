3898ea0d8fb388ab8b984cdee8380b1ee3c913af
Lokesh Sattineni <lokeshsattineni018@gmail.com>
2026-05-09 10:49:26 +0530

    Fix User model pre-save hook TypeError

--- server/models/User.js
+++ server/models/User.js
@@ -86,13 +86,12 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.pre("save", function (next) {
